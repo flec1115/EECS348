ba25bb49425d06271fd5a04e55fc9cf57a04978c
flec1115 <balandranf11@gmail.com>
2024-10-09 12:10:29 -0500

    Changed array of strings to floats, returns to functions

--- lab5/problem1/sales_report.c
+++ lab5/problem1/sales_report.c
@@ -4,25 +4,27 @@
 #define MAX_LINES 12
 #define MAX_LEN 100
 
-void print_monthly(){
+void print_monthly(float sales[12], char months[12][MAX_LEN]){
 
 }
 
-void print_summary(){
+void print_summary(float sales[12], char months[12][MAX_LEN]){
 
 }
 
-void six_month_avg(){
+void six_month_avg(float sales[12], char months[12][MAX_LEN]){
 
 }
 
-void print_reprot(){
+void print_reprot(float sales[12], char months[12][MAX_LEN]){
 
 }
 
 int main(){
   FILE* fptr;
   char monthly_sales[MAX_LINES][MAX_LEN];
+  float real_sales[12];
+  char months[12][MAX_LEN] = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "Octoberr", "November", "December"};
 
   fptr = fopen("sales.txt", "r");
 
@@ -33,13 +35,15 @@ int main(){
 
   int line = 0; 
 
-  while (!feof(fptr) && !ferror(fptr))
-    if (fgets(monthly_sales[line], MAX_LEN, fptr) != NULL)
-      line++;
-  fclose(fptr);
+  while (!feof(fptr) && !ferror(fptr))  // while loop contiunous while not the end of line and no errors
+    if (fgets(monthly_sales[line], MAX_LEN, fptr) != NULL) //apends lines to list 
+      line++;  // increments lines 
+  fclose(fptr);  //close files
 
-  for (int i = 0; i < line; i++)
-    printf("%s", monthly_sales[i]);
+  for (int i = 0; i < line; i++){
+    real_sales[i] = atof(monthly_sales[i]);
+    printf("%.2f\n", real_sales[i]);
+  }
   
   return 0;
 
